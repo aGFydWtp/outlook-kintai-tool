ee0ecf6e16cc3900f8ff555df2de6c31212abed9
aGFydWtp <haruki@censor.space>
2024-06-28 13:46:49 +0000

    fix: update lockfile

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,15 @@
+import { MetaFunction } from "@remix-run/cloudflare";
+
 import { Index as Home } from "@/components/top";
+
+export const meta: MetaFunction = () => {
+  const title = "きょうのよてい⛅";
+  return [
+    { title: title },
+    { property: "og:title", content: title },
+    { property: "og:site_name", content: title },
+  ];
+};
 
 export default function Index() {
   return <Home />;
